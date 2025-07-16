39e17b208e710cdf18c9c27d355a16ffd4a6801d
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-16 14:12:48 -0300

    Add: DataInputExercise(Discover Circle Area Exercise)

--- src/DataInputExercise.java
+++ src/DataInputExercise.java
@@ -1,12 +1,14 @@
 import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class DataInputExercise {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int x,y;
-
+        /* Integers Sum
+        int x, y;
         try {
             System.out.print("Write an integer: ");
             x = sc.nextInt();
@@ -17,5 +19,24 @@ public class DataInputExercise {
         } catch (InputMismatchException ex){
             throw new IllegalArgumentException("ONLY INTEGERS NUMBERS ARE ACCEPTED!!");
         }
+        */
+
+        /*
+            Discovering Circle Area
+
+            double radius;
+            try {
+
+                System.out.print("Write the radius of an circle: ");
+                radius = sc.nextDouble();
+
+                double area = Math.PI * (radius * radius);
+
+                System.out.printf("The circle area is: %.4f", area);
+
+            } catch (InputMismatchException ex){
+                throw new IllegalArgumentException("ONLY NUMBERS ARE ACCEPTED!!");
+            }]
+        */
     }
 }
