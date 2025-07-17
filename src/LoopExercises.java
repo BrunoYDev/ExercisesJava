c9be51c30bb87d9f507f5e86554aa07993ea1676
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-17 16:18:33 -0300

    Add: LoopExercises(Verify Password with while loop)

---
+++ src/LoopExercises.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LoopExercises {
+
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        /* Verify if a password is correct Loop While
+            String password;
+
+            System.out.print("Insert the password: ");
+            password = sc.nextLine();
+
+            while(!password.equals("2002")){
+                System.out.println("Incorrect Password!");
+                System.out.print("Try again: ");
+                password = sc.nextLine();
+            }
+
+            System.out.println("Correct Password, you´re in :D ");
+         */
+
+    }
+}
