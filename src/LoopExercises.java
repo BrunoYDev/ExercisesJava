0f8523ca32dd5ab67556ffcb2f6e3eab1a7a943b
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-17 16:55:01 -0300

    Add: LoopExercises(Gas station terminal with while loop)

--- src/LoopExercises.java
+++ src/LoopExercises.java
@@ -7,6 +7,7 @@ public class LoopExercises {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        // Exercises Using While
         /* Verify if a password is correct Loop While
             String password;
 
@@ -43,6 +44,54 @@ public class LoopExercises {
                 }
 
             }
+         */
+
+        /* Using while to create a gas station like console
+            int choice;
+            int alc = 0;
+            int gas = 0;
+            int dis = 0;
+
+            System.out.println("Welcome to gas station!!");
+            System.out.println("Choose an option: ");
+            System.out.println("1: Alcohol");
+            System.out.println("2: Gasoline");
+            System.out.println("3: Diesel");
+            System.out.println("4: End Program");
+            choice = sc.nextInt();
+
+            while(choice != 4){
+                switch (choice){
+                    case 1:
+                        alc += 1;
+                        break;
+                    case 2:
+                        gas += 1;
+                        break;
+                    case 3:
+                        dis += 1;
+                        break;
+                    default:
+                        System.out.println("Invalid choice!");
+                        break;
+                }
+                System.out.println("----------------------------------------------------------------------------");
+
+                System.out.println("Welcome to gas station!!");
+                System.out.println("Choose an option: ");
+                System.out.println("1: Alcohol");
+                System.out.println("2: Gasoline");
+                System.out.println("3: Diesel");
+                System.out.println("4: End Program");
+                choice = sc.nextInt();
+            }
+
+            System.out.println("----------------------------------------------------------------------------");
+            System.out.println("End of the day!");
+            System.out.println("Total sells: ");
+            System.out.println("Alcohol: " + alc);
+            System.out.println("Gasoline: " + gas);
+            System.out.println("Diesel: " + dis);
          */
 
     }
