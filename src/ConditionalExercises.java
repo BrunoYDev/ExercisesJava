4ef9d35ed7cc7a410b9cea29da441c20e69e2d0b
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-17 14:17:13 -0300

    Add: ConditionalExercises(Odds or Evens Exercise)

---
+++ src/ConditionalExercises.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ConditionalExercises {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        /* Odds or Evens
+            int num;
+
+            System.out.print("Insert an integer number: ");
+            num = sc.nextInt();
+
+            if(num % 2 == 0){
+                System.out.println("This number is an Odd Number!");
+            }else{
+                System.out.println("This number is an Even Number!");
+            }
+       */
+
+
+
+    }
+}
