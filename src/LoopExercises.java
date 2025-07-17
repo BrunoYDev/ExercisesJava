72ae1b8b1bd38d2812d90c3636f52513a03da708
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-17 16:36:33 -0300

    Add: LoopExercises(Discover the quadrant of a given point in the cartesian plane)

--- src/LoopExercises.java
+++ src/LoopExercises.java
@@ -22,5 +22,28 @@ public class LoopExercises {
             System.out.println("Correct Password, you´re in :D ");
          */
 
+        /* Receive two coordinates X and Y and give the quadrant of the point the cartesian plane
+            int x = 1, y = 1;
+
+            while(x != 0 && y != 0){
+                System.out.print("Write an X position (e.g: 3): ");
+                x = sc.nextInt();
+
+                System.out.print("Write an Y position (e.g: 2): ");
+                y = sc.nextInt();
+
+                if(x > 0 && y > 0){
+                    System.out.println("Second");
+                }else if (x > 0 && y < 0){
+                    System.out.println("Fourth");
+                }else if (x < 0 && y > 0){
+                    System.out.println("First");
+                }else{
+                    System.out.println("Third");
+                }
+
+            }
+         */
+
     }
 }
