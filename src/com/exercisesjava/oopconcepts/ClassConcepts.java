2e8cc8f971a883bacc08f92fecb8a52681b87998
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-21 15:57:48 -0300

    Add: ClassConcepts.java( creating a product instance )

--- src/com/exercisesjava/oopconcepts/ClassConcepts.java
+++ src/com/exercisesjava/oopconcepts/ClassConcepts.java
@@ -4,6 +4,11 @@ public class ClassConcepts {
 
     public static void main(String[] args){
 
+        Product banana = new Product("Banana", 5.30);
+
+        banana.AddProducts(10);
+
+        System.out.printf("Total Banana Value: $ %.2f", banana.TotalValueInStock());
     }
 }
 
