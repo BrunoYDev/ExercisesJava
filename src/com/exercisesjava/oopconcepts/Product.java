17dcc53763f628c297d9e60438396745917dc808
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-21 15:48:39 -0300

    Add: Product.java Methods and Attributes

--- src/com/exercisesjava/oopconcepts/Product.java
+++ src/com/exercisesjava/oopconcepts/Product.java
@@ -2,4 +2,19 @@ package com.exercisesjava.oopconcepts;
 
 public class Product {
 
+    String Name;
+    double Price;
+    private int Quantity;
+
+    public double TotalValueInStock(){
+        return Price * Quantity;
+    }
+
+    public void AddProducts(int Quantity){
+        this.Quantity += Quantity;
+    }
+
+    public void RemoveProducts(int Quantity){
+        this.Quantity -= Quantity;
+    }
 }
