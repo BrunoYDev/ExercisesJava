9325d734c9531c89823b26d821b742bd73b22928
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-08-08 16:46:05 -0300

    Create: new exercise inheritance and polymorphism

---
+++ src/com/exercisesjava/inheritanceandpolymorphism/entities/Employee.java
@@ -0,0 +1,33 @@
+package com.exercisesjava.inheritanceandpolymorphism.entities;
+
+public class Employee {
+
+    private String name;
+    private Integer hours;
+    private Double valuePerHour;
+
+    public Employee() {
+    }
+
+    public Employee(String name, Integer hours, Double valuePerHour) {
+        this.name = name;
+        this.hours = hours;
+        this.valuePerHour = valuePerHour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public Double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public Double payment(){
+        return valuePerHour * hours;
+    }
+}
---
+++ src/com/exercisesjava/inheritanceandpolymorphism/entities/OutsourcedEmployee.java
@@ -0,0 +1,24 @@
+package com.exercisesjava.inheritanceandpolymorphism.entities;
+
+public class OutsourcedEmployee extends Employee{
+
+    private Double additionalCharge;
+
+    public OutsourcedEmployee() {
+        super();
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    @Override
+    public Double payment(){
+        return getValuePerHour() * getHours() + (additionalCharge * 1.1);
+    }
+}
